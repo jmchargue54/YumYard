a05bd1872d2277dd4bbde7d89c58d48829522e97
jmchargue54 <jmchargue54@gmail.com>
2025-03-17 16:57:05 -0600

    tested and api pull

--- YumYard/src/App.svelte
+++ YumYard/src/App.svelte
@@ -1,7 +1,25 @@
 <script>
+  import { onMount } from 'svelte';
+  import { getRandomRecipes } from './js/api.js';
+  import './css/main.css';
 
+  let recipes = [];
+
+  onMount(async () => {
+      recipes = await getRandomRecipes();
+  });
 </script>
 
 <main>
-  <a href="./html/index.html">Home</a>
+  <h1>Random Recipes</h1>
+  <div id="recipe-container">
+      {#each recipes as recipe}
+          <div class="recipe">
+              <h2>{recipe.title}</h2>
+              <img src={recipe.image} alt={recipe.title} />
+              <p>{@html recipe.summary}</p>
+              <a href={recipe.sourceUrl} target="_blank">View Recipe</a>
+          </div>
+      {/each}
+  </div>
 </main>
---
+++ YumYard/src/js/api.js
@@ -0,0 +1,13 @@
+const api = 'https://api.spoonacular.com';
+const key = '6193542d0b6b4048852bcb497aecb59b';
+
+export async function getRandomRecipes(number = 5) {
+    try {
+        const response = await fetch(`${api}/recipes/random?apiKey=${key}&number=${number}`);
+        const data = await response.json();
+        return data.recipes; // Return recipes to be used in Svelte
+    } catch (error) {
+        console.error('Error fetching recipes:', error);
+        return [];
+    }
+}
