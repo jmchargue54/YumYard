f26dab57604e0e7598755397e0444f5dd59ffadd
jmchargue54 <jmchargue54@gmail.com>
2025-03-27 14:23:22 -0600

    starting

--- src/js/components/TmrRecipes.svelte
+++ src/js/components/TmrRecipes.svelte
@@ -13,20 +13,20 @@
   <main>
     <h2>Tommorrow</h2>
         <div id="recipeContainer">
-            {#each recipes as recipe}
+            <!-- {#each recipes as recipe}
                 <div class="recipe">
                   <img src={recipe.image} alt={recipe.title} />
                     <h2>{recipe.title}</h2>
                     <a href={recipe.sourceUrl} target="_blank">View Recipe</a>
                     <button id="addButton">Add to Favorites</button>
                   </div>
-            {/each}
-        <!-- <div class="recipe">Recipe Here</div>
+            {/each} -->
         <div class="recipe">Recipe Here</div>
         <div class="recipe">Recipe Here</div>
         <div class="recipe">Recipe Here</div>
         <div class="recipe">Recipe Here</div>
-   -->
+        <div class="recipe">Recipe Here</div>
+  
       </div>
   </main>
   
