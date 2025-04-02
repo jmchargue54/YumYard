df40bc7fc82c343e2493dd0c8c117a2a9a9da98a
galliem <gar22094@byui.edu>
2025-04-02 13:05:36 -0600

    fixed errors with help from professor

--- src/js/components/Header.svelte
+++ src/js/components/Header.svelte
@@ -30,9 +30,9 @@ nav {
 <div>
   <h1>Random Recipes</h1>
   <nav>
-    <a href="index.html">Main</a>
-    <a href="html/planner.html">Planner</a>
-    <a href="html/favorite.html">Favorites</a>
-    <a href="html/ideas.html">Ideas</a>
+    <a href="/index.html">Main</a>
+    <a href="/html/planner.html">Planner</a>
+    <a href="/html/favorite.html">Favorites</a>
+    <a href="/html/ideas.html">Ideas</a>
   </nav>
 </div>
