fad3aeda77143b2a2206208f940f4ccd8e89d1cf
jmchargue54 <jmchargue54@gmail.com>
2025-04-03 14:19:41 -0600

    imported recipes

--- src/js/components/favorites.svelte
+++ src/js/components/favorites.svelte
@@ -1,14 +1,21 @@
 <script>
     import { favorites } from '../stores.js';
+
+    function RemoveFromFavorites(recipe) {
+        favorites.update((currentFavorites) => {
+            return currentFavorites.filter(fav => fav.title !== recipe.title);
+        });
+    }
   </script>
   
   <h2>Favorites List</h2>
   <ul>
     {#each $favorites as recipe}
-      <li>
-        <img src={recipe.image} alt={recipe.title} width="50" />
-        <a href={recipe.sourceUrl} target="_blank">{recipe.title}</a>
-      </li>
+      <div class="recipe">
+          <img src={recipe.image} alt={recipe.title} />
+          <h2>{recipe.title}</h2>
+          <a href={recipe.sourceUrl} target="_blank">View Recipe</a>
+          <button class="removeButton" on:click={() => RemoveFromFavorites(recipe)}>Add to Favorites</button>
+      </div>
     {/each}
   </ul>
-  
